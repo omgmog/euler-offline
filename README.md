dd70cbf289afd6cf6ec72941fb21ff38bf6122a7
Max Glenister <moglenstar@gmail.com>
2016-04-28 14:10:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Euler Offline
+
+A quick node script that offlines the [Project Euler](https://projecteuler.net) questions, so you can work on them anywhere.
+
+This uses [Jam API](http://www.jamapi.xyz/) to scrape the titles and question data from Project Euler.
+
+## Usage
+
+```shell
+$ npm install require fs
+$ node index.js
+```
+
+This will create the `questions/problem-xxx.js` files, which you can then work through in your editor of choice.
+
+## What is Project Euler?
+
+Project Euler is a series of challenging mathematical/computer programming problems that will require more than just mathematical insights to solve. Although mathematics will help you arrive at elegant and efficient methods, the use of a computer and programming skills will be required to solve most problems.
+
+The motivation for starting Project Euler, and its continuation, is to provide a platform for the inquiring mind to delve into unfamiliar areas and learn new concepts in a fun and recreational context.
---
+++ index.js
@@ -0,0 +1,43 @@
+var request = require('request');
+var fs = require('fs');
+
+var problemsCount = 547;
+var dir = './questions/';
+
+for (var i=1; i<=problemsCount; i++) {
+  getProblem(i);
+}
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function getProblem(id) {
+  id = pad(id, problemsCount.toString().length);
+
+  request.post(
+    'http://www.jamapi.xyz',
+    {
+      form: {
+        url: 'https://projecteuler.net/problem=' + id,
+        json_data: '{"title": "h2", "problem": ".problem_content"}'
+      }
+    },
+    function (err, res, body) {
+      if (!err) {
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir)
+        }
+        var parsed = JSON.parse(body);
+        var filename = dir + 'problem-'+id+'.js';
+        var content = '/*\n' + parsed.title + '\n' + parsed.problem + '*/\n\n';
+        fs.writeFile(filename, content, function (err) {
+          if (err) return console.log(err);
+          console.log('Wrote ', filename);
+        });
+      }
+    }
+  );
+}
